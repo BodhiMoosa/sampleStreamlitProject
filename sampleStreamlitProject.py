1648ef926bde57249e952223b0c650670f72e2a9
Moosa <bodhimoosa@gmail.com>
2022-10-19 15:27:41 -0400

    final commit haha

--- sampleStreamlitProject.py
+++ sampleStreamlitProject.py
@@ -5,3 +5,10 @@ st.write("""
 This is a sample project.
 """)
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write("You can organize your app by columns if you want")
+
+with col3:
+    st.write("But then again, you don't have to. It's just a suggestion")
